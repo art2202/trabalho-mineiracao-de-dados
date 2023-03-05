f5826005f84a0485e8774e59abaf94fe3c75a635
Arthur Rodrigues <arthurrodrigues29@gmail.com>
2023-03-04 23:27:20 -0300

    alterando knn para usar y com 1 dimensao

--- knn.py
+++ knn.py
@@ -1,19 +1,17 @@
 import pandas as pd
-from sklearn import preprocessing
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 
 datasetLearning = pd.read_csv("new_dataset.csv")
-print(datasetLearning.columns.size)
+
 X = datasetLearning.iloc[:, :].values
 
 # axis=1 significa coluna, drop("causas_resumidas_Condutor", axis=1) = apagar coluna causas_resumidas_condutor
-x_dataset = datasetLearning.drop("causas_resumidas_Condutor", axis=1).drop("causas_resumidas_Outras Causas", axis=1).drop("causas_resumidas_Pista", axis=1).drop("causas_resumidas_Veiculo", axis=1)
+x_dataset = datasetLearning.drop("causas_resumidas_Condutor", axis=1)
 
-y_dataset = pd.DataFrame({'causas_resumidas_Condutor':X[:,48]}).join(pd.DataFrame({'causas_resumidas_Outras Causas':X[:,49]}))\
-    .join(pd.DataFrame({'causas_resumidas_Pista':X[:,50]})).join(pd.DataFrame({'causas_resumidas_Veiculo':X[:,51]}))
+y_dataset = datasetLearning['causas_resumidas_Condutor']
 
 x_dataset_train, x_dataset_test, y_dataset_train, y_dataset_test = train_test_split(x_dataset, y_dataset, test_size=0.2)
 
@@ -28,5 +26,8 @@ knn.fit(x_dataset_train, y_dataset_train)
 result = knn.predict(x_dataset_test)
 
 accuracy = accuracy_score(y_dataset_test, result)
+f1 = f1_score(result, y_dataset_test, average="weighted")
+
 print("Accuracy:", accuracy)
+
 

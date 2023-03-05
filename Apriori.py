71842c7e3d572a118893b4d7cc1382a3fc9723db
Arthur Rodrigues <arthurrodrigues29@gmail.com>
2023-03-05 16:57:43 -0300

    dataset textual

--- Apriori.py
+++ Apriori.py
@@ -3,7 +3,7 @@ import pandas as pd
 from apyori import apriori
 
 # carregando os dados do arquivo CSV em um DataFrame
-dados = pd.read_csv('new_dataset.csv')
+dados = pd.read_csv('dataset_textual.csv')
 
 # convertendo os dados do DataFrame em uma lista de listas
 dados = dados.values.tolist()

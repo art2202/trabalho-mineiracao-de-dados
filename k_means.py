c2ae598d1ab9e0588ac677a8c206fcab0bd95e6f
Arthur Rodrigues <arthurrodrigues29@gmail.com>
2023-03-05 11:56:51 -0300

    nomeando colunas do kmeans de acordo com as tabelas do new_dataset

--- k_means.py
+++ k_means.py
@@ -2,10 +2,10 @@ import pandas as pd
 from sklearn.cluster import KMeans
 
 # Lê o arquivo CSV em um DataFrame do pandas
-data = pd.read_csv('new_dataset.csv.csv')
+data = pd.read_csv('new_dataset.csv')
 
 # Seleciona as colunas de interesse para o agrupamento
-X = data[['coluna1', 'coluna2', 'coluna3']]
+X = data[['causas_resumidas_Condutor', 'causas_resumidas_Outras Causas', 'causas_resumidas_Pista', 'causas_resumidas_Veiculo']]
 
 # Cria um modelo k-means com 4 clusters
 kmeans = KMeans(n_clusters=4)
